67520026fc1efb77da6354c99edef63e1e32638e
clara <claraa.zip@gmail.com>
2026-03-05 19:10:45 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -7,17 +7,18 @@ operador = input("Digite a opção desejada (1 para soma, 2 para subtração, 3 
 num2 = int(input("Digite o segundo número:"))
 
 if operador == "1":
-    operacao = num1 + num2
+    operação = num1 + num2
 elif operador == "2":
-    operacao = num1 - num2
+    operação = num1 - num2
 elif operador == "3":
-    operacao = num1 / num2
+    operação = num1 / num2
 elif operador == "4":
-    operacao = num1 * num2
+    operação = num1 * num2
 else:
-    operacao = "Operador Inválido, Use: 1, 2, 3 ou 4."
+    operação = "Operador Inválido, Use: 1, 2, 3 ou 4."
 print ("Resultado:")
-print (operacao)
+print (operação)
+
 
 
 
